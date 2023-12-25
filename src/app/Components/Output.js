1676e184f724accd096991dcd94d11f6b7ed82a2
Mohit <81846020+MohitGupta14@users.noreply.github.com>
2023-12-25 20:56:53 +0530

    Updating link

--- src/app/Components/Output.js
+++ src/app/Components/Output.js
@@ -4,8 +4,8 @@ import ReactMarkdown from 'react-markdown';
 const Output = ({ generatedText }) => {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-gray-900 p-6 border-t border-gray-300 overflow-y-auto max-h-80 min-h-0 rounded-t-lg flex flex-col items-center justify-center">
-      <ReactMarkdown className="text-white" children={generatedText} />
-      <span className="text-purple-500 text-lg mt-2">&#9733; Give us a star <a href="https://www.github.com" className="text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Github</a></span>
+      <ReactMarkdown className="text-white">{generatedText}</ReactMarkdown>
+      <span className="text-purple-500 text-lg mt-2">&#9733; Give us a star <a href="https://github.com/MohitGupta14/prompt-prodigy" className="text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Github</a></span>
     </div>
   );
 };
